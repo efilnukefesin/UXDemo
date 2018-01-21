ceba7fcf944cde9ef7e91ae50ecf990ee34c1f88
efilnukefesin <efil.threat@googlemail.com>
2018-01-21 14:46:52 +0100

    wired some controls

---
+++ UXDemo.Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Wpf.UXDemo.Models
+{
+    public class UserModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public UserModel()
+        {
+            this.Firstname = "Nigel";
+            this.Lastname = "Lotze";
+        }
+    }
+}
---
+++ UXDemo.ViewModels/UserInfoViewModel.cs
@@ -0,0 +1,34 @@
+﻿using NET.efilnukefesin.Wpf.UXDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Wpf.UXDemo.ViewModels
+{
+    public class UserInfoViewModel : BaseViewModel
+    {
+        #region Properties
+
+        public UserModel User { get; set; }
+
+        #endregion Properties
+
+        #region Construction
+
+        public UserInfoViewModel()
+            : base()
+        {
+            this.User = new UserModel();
+        }
+
+        #endregion Construction
+
+        #region Methods
+
+        #endregion Methods
+
+        #region Events
+
+        #endregion Events
+    }
+}
