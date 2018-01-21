d441c62f90dcb6fb7dd2964753b31dfbf7855968
efilnukefesin <efil.threat@googlemail.com>
2018-01-21 12:57:04 +0100

    added  Standard Libs and Fody

---
+++ UXDemo.ViewModels/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using PropertyChanged;
+
+namespace NET.efilnukefesin.Wpf.UXDemo.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Construction
+
+        #endregion Construction
+
+        #region Methods
+
+        #endregion Methods
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+    }
+}
