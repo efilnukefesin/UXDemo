73be62faac53000c7ff3ac29c6e9eb7b94588964
efilnukefesin <efil.threat@googlemail.com>
2018-01-22 00:06:40 +0100

    styled a bit

--- UXDemo.Models/UserModel.cs
+++ UXDemo.Models/UserModel.cs
@@ -8,14 +8,26 @@ namespace NET.efilnukefesin.Wpf.UXDemo.Models
     {
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public string Nickname { get; set; }
+
+        //TODO: put into Level Model
+
         public int Experience { get; set; }
+        public string NextTitle { get; set; }
+        public string Title { get; set; }
+        public int MaxExperience { get; set; }
+        public int NeededExperience { get { return this.MaxExperience - this.Experience; } }
         public Uri ImageUri { get; set; }
 
         public UserModel()
         {
+            this.Nickname = "Lotzinator";
             this.Firstname = "Nigel";
             this.Lastname = "Lotze";
             this.Experience = 10;
+            this.MaxExperience = 100;
+            this.Title = "AwesomeTitle";
+            this.NextTitle = "MoreAwesomeTitle";
         }
     }
 }
