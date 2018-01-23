31e6e1ec0d885bdce2933dc1e37d418c45660eb6
efilnukefesin <efil.threat@googlemail.com>
2018-01-23 21:38:08 +0100

    added the settings view model and changed version numbers

---
+++ UXDemo.ViewModels/SettingsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Wpf.UXDemo.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+    }
+}
--- UXDemo/Properties/AssemblyInfo.cs
+++ UXDemo/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("UXDemo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Demo for a possible gamified UX")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("efilnukefesin.NET")]
 [assembly: AssemblyProduct("UXDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © efilnukefesin.NET 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
