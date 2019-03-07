83133c778a92e7d585152d8fa7a1e1ed11d88b03
efilnukefesin <efil.threat@googlemail.com>
2019-03-07 22:45:06 +0100

    got the style change to work!!!!!

---
+++ UXDemo.Converters/StyleMultiConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NET.efilnukefesin.Apps.UXDemo.Converters
+{
+    public class StyleMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string dataValue = values[0] as string;
+            Style firstStyle = values[1] as Style;
+            Style secondStyle = values[2] as Style;
+
+            //return dataValue.Equals("cust2") ? firstStyle : secondStyle;
+            if (dataValue.Equals("MaterialDesignRaisedLightButtonEdgy"))
+            {
+                return firstStyle;
+            }
+            else
+            {
+                return secondStyle;
+            }     
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
