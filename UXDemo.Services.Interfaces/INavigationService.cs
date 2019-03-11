363a41a629c5132ef1da041fa9e00ede85d5e7d3
efilnukefesin <efil.threat@googlemail.com>
2019-03-11 23:27:44 +0100

    added rumps for Navigation Services

---
+++ UXDemo.Services.Interfaces/INavigationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Apps.UXDemo.Services.Interfaces
+{
+    public interface INavigationService
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+
+        #endregion Methods
+
+        #region Events
+
+        #endregion Events
+    }
+}
---
+++ UXDemo.Services/NavigationService.cs
@@ -0,0 +1,26 @@
+﻿using NET.efilnukefesin.Apps.UXDemo.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NET.efilnukefesin.Apps.UXDemo.Services
+{
+    public class NavigationService : INavigationService
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Construction
+
+        #endregion Construction
+
+        #region Methods
+
+        #endregion Methods
+
+        #region Events
+
+        #endregion Events
+    }
+}
