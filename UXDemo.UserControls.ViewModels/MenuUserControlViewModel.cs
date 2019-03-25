70d72757cec1656dc362871992250714767da56e
efilnukefesin <efil.threat@googlemail.com>
2019-03-25 23:12:26 +0100

    fixed design viewmodels for MenuUserControl

--- UXDemo.UserControls.ViewModels/MenuUserControlViewModel.cs
+++ UXDemo.UserControls.ViewModels/MenuUserControlViewModel.cs
@@ -40,7 +40,14 @@ namespace NET.efilnukefesin.Apps.UXDemo.UserControls.ViewModels
 
         public MenuUserControlViewModel()
         {
-            this.navigationService = DiManager.GetInstance().Resolve<INavigationService>();  //TODO: clean up, catch exception
+            if (DiManager.GetInstance().IsRegistered<INavigationService>())
+            {
+                this.navigationService = DiManager.GetInstance().Resolve<INavigationService>();  //TODO: clean up, catch exception
+            }
+            else
+            {
+                this.navigationService = null;
+            }
             this.SearchInfo = new SearchBoxUserControlViewModel() { Text = "Something2!" };
             this.VersionInfo = new VersionInfoUserControlViewModel() { Version = new VersionModel(new Version(1, 1), new DateTimeOffset(2019, 03, 04, 23, 59, 59, new TimeSpan(-1, 0, 0))) };
             this.UserInfo = new UserInfoUserControlViewModel() { User = new UserModel() { Firstname = "Nigel", Lastname = "Lotze", Nickname = "Lotzinator", CurrentLevel = new LevelModel() { MinExperience = 100, MaxExperience = 102, Title = "Grand Geek" }, Experience = 101 } };
